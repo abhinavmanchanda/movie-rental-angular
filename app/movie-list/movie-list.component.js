5eb28ffb0935a941ad32cd94ddcbfbd5d80c5ad0
Abhinav <abhinav.manchanda@gmail.com>
2017-11-26 15:00:03 +0530

    Added logic and tests to the controller to retrieve movies from the service.

--- app/movie-list/movie-list.component.js
+++ app/movie-list/movie-list.component.js
@@ -4,18 +4,7 @@ angular.
   module('movieList').
   component('movieList', {
     templateUrl: 'movie-list/movie-list.template.html',
-    controller: function MovieListController() {
-      this.movies = [
-        {
-          name: 'The Shawshank Redemption',
-          snippet: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'
-        }, {
-          name: 'The Godfather',
-          snippet: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.'
-        }, {
-          name: 'The Godfather: Part II',
-          snippet: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.'
-        }
-      ];
+    controller: function MovieListController(movieListService) {
+      this.movies = movieListService.query();
     }
   });
--- app/movie-list/movie-list.component.spec.js
+++ app/movie-list/movie-list.component.spec.js
@@ -1,18 +1,42 @@
 'use strict';
 
-describe('movieList', function() {
+describe('MovieListController', function() {
+
+  var movieListController;
+  var movieListService;
+  var stubMovieListData = [
+    {
+      name: 'Gunda',
+      snippet: 'Highest rated movie of all time.'
+    }, {
+      name: 'Tabaahi - The Destroyer',
+      snippet: 'Don\'t awaken his dark side.'
+    }, {
+      name: 'Disco Dancer',
+      snippet: 'Too cool for school.'
+    },  {
+      name: 'The Godfather: Part II',
+      snippet: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.'
+    }
+  ]
 
   beforeEach(module('movieList'));
 
-  // Test the controller
-  describe('MovieListController', function() {
+  beforeEach(function() {
+    jasmine.addCustomEqualityTester(angular.equals);
+  });
 
-    it('should create a `movies` model with 3 movies', inject(function($componentController) {
-      var controller = $componentController('movieList');
+  beforeEach(inject(function($componentController) {
+    movieListService = {};
+    movieListService.query = function(){
+      return stubMovieListData;
+    };
+    movieListController = $componentController('movieList', {movieListService: movieListService});
+  }));
 
-      expect(controller.movies.length).toBe(3);
-    }));
-
+  it('should create a `movies` model with 4 movies', function() {
+    expect(movieListController.movies.length).toBe(4);
+    expect(movieListController.movies[1].name).toBe("Tabaahi - The Destroyer");
   });
 
 });
