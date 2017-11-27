e1c9540a48d197bd85803718642ce42cbc9f5549
Abhinav <abhinav.manchanda@gmail.com>
2017-11-27 15:28:42 +0530

    Added logic to reserve a movie, frontend code yet to be added.

--- app/movie-list/movie-list.component.js
+++ app/movie-list/movie-list.component.js
@@ -5,6 +5,10 @@ angular.
   component('movieList', {
     templateUrl: 'movie-list/movie-list.template.html',
     controller: function MovieListController(movieListService) {
-      this.movies = movieListService.query();
+      var self = this;
+      self.movies = movieListService.query();
+      self.reserveItem = function(currentMovie){
+        currentMovie.reserved = !(currentMovie.reserved);
+      };
     }
   });
--- app/movie-list/movie-list.component.spec.js
+++ app/movie-list/movie-list.component.spec.js
@@ -4,21 +4,6 @@ describe('MovieListController', function() {
 
   var movieListController;
   var movieListService;
-  var stubMovieListData = [
-    {
-      name: 'Gunda',
-      snippet: 'Highest rated movie of all time.'
-    }, {
-      name: 'Tabaahi - The Destroyer',
-      snippet: 'Don\'t awaken his dark side.'
-    }, {
-      name: 'Disco Dancer',
-      snippet: 'Too cool for school.'
-    },  {
-      name: 'The Godfather: Part II',
-      snippet: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.'
-    }
-  ]
 
   beforeEach(module('movieList'));
 
@@ -27,6 +12,21 @@ describe('MovieListController', function() {
   });
 
   beforeEach(inject(function($componentController) {
+    var stubMovieListData = [
+      {
+        name: 'Gunda',
+        snippet: 'Highest rated movie of all time.'
+      }, {
+        name: 'Tabaahi - The Destroyer',
+        snippet: 'Don\'t awaken his dark side.'
+      }, {
+        name: 'Disco Dancer',
+        snippet: 'Too cool for school.'
+      },  {
+        name: 'The Godfather: Part II',
+        snippet: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.'
+      }
+    ]
     movieListService = {};
     movieListService.query = function(){
       return stubMovieListData;
@@ -37,6 +37,20 @@ describe('MovieListController', function() {
   it('should create a `movies` model with 4 movies', function() {
     expect(movieListController.movies.length).toBe(4);
     expect(movieListController.movies[1].name).toBe("Tabaahi - The Destroyer");
+  });
+
+  it('should reserve an item when reserve for that item is called', function(){
+    var secondMovie = movieListController.movies[1];
+    movieListController.reserveItem(secondMovie);
+    expect(secondMovie.reserved).toBe(true);
+  });
+
+  it('should unreserve an item when reserve for that item is called, and it is already reserved', function(){
+    var secondMovie = movieListController.movies[1];
+    movieListController.reserveItem(secondMovie);
+    expect(secondMovie.reserved).toBe(true);
+    movieListController.reserveItem(secondMovie);
+    expect(secondMovie.reserved).toBe(false);
   });
 
 });
