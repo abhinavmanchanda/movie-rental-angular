a7af1dc085db88d7e3521b5d5382bfa97c8f219e
Abhinav <abhinav.manchanda@gmail.com>
2017-11-27 13:05:42 +0530

    Added end to end test for displaying movie list.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,12 +1,17 @@
 'use strict';
 
-// Angular E2E Testing Guide:
-// https://docs.angularjs.org/guide/e2e-testing
-
-describe('My app', function() {
+describe('Movie Rental Application', function() {
 
   beforeEach(function() {
     browser.get('index.html');
+  });
+
+  it("should display the list of movies", function(){
+    var movieList = element.all(by.repeater('movie in $ctrl.movies'));
+    expect(movieList.count()).toBe(6);
+
+    var thirdMovieName = element(by.repeater('movie in $ctrl.movies').row(2).column('movie.name')).getText();
+    expect(thirdMovieName).toEqual('The Godfather: Part II')
   });
 
 });
