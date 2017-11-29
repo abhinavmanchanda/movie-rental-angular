761e328a607a347c64440753ea1f174c267be64c
Abhinav <abhinav.manchanda@gmail.com>
2017-11-29 18:57:25 +0530

    Removed service for assignment 2, removed the e2e test for the skeleton. The unit test for controller is present.

--- app/movie-list/movie-list.component.spec.js
+++ app/movie-list/movie-list.component.spec.js
@@ -3,40 +3,16 @@
 describe('MovieListController', function() {
 
   var movieListController;
-  var movieListService;
 
   beforeEach(module('movieList'));
 
-  beforeEach(function() {
-    jasmine.addCustomEqualityTester(angular.equals);
-  });
-
   beforeEach(inject(function($componentController) {
-    var stubMovieListData = [
-      {
-        name: 'Gunda',
-        snippet: 'Highest rated movie of all time.'
-      }, {
-        name: 'Tabaahi - The Destroyer',
-        snippet: 'Don\'t awaken his dark side.'
-      }, {
-        name: 'Disco Dancer',
-        snippet: 'Too cool for school.'
-      },  {
-        name: 'The Godfather: Part II',
-        snippet: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.'
-      }
-    ]
-    movieListService = {};
-    movieListService.query = function(){
-      return stubMovieListData;
-    };
-    movieListController = $componentController('movieList', {movieListService: movieListService});
+    movieListController = $componentController('movieList');
   }));
 
   it('should create a `movies` model with 4 movies', function() {
-    expect(movieListController.movies.length).toBe(4);
-    expect(movieListController.movies[1].name).toBe("Tabaahi - The Destroyer");
+    expect(movieListController.movies.length).toBe(6);
+    expect(movieListController.movies[4].name).toBe("Tabaahi - The Destroyer");
   });
 
   it('should reserve an item when reserve for that item is called', function(){
