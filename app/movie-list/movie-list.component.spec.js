bc9b4c7b0b2d4ba1b9d3c17ebaf207f568c2bc8c
Abhinav <abhinav.manchanda@gmail.com>
2017-12-05 15:13:51 +0530

    Removed all unit tests except for a sample one.

--- app/movie-list/movie-list.component.spec.js
+++ app/movie-list/movie-list.component.spec.js
@@ -10,32 +10,9 @@ describe('MovieListController', function() {
     movieListController = $componentController('movieList');
   }));
 
-  it('should create a `movies` model with 4 movies', function() {
+  it('should create a `movies` model with 6 movies', function() {
     expect(movieListController.movies.length).toBe(6);
     expect(movieListController.movies[4].name).toBe("Tabaahi - The Destroyer");
   });
-
-  it('should reserve an item when reserve for that item is called', function(){
-    var secondMovie = movieListController.movies[1];
-    movieListController.reserveItem(secondMovie);
-    expect(secondMovie.reserved).toBe(true);
-  });
-
-  it('should unreserve an item when reserve for that item is called, and it is already reserved', function(){
-    var secondMovie = movieListController.movies[1];
-    movieListController.reserveItem(secondMovie);
-    expect(secondMovie.reserved).toBe(true);
-    movieListController.reserveItem(secondMovie);
-    expect(secondMovie.reserved).toBe(false);
-  });
-
-  it('should display the count of selected movies', function(){
-    var secondMovie = movieListController.movies[1];
-    var fifthMovie = movieListController.movies[4];
-    movieListController.reserveItem(secondMovie);
-    expect(movieListController.reservedItemCount()).toBe(1);
-    movieListController.reserveItem(fifthMovie);
-    expect(movieListController.reservedItemCount()).toBe(2);
-  })
 
 });
