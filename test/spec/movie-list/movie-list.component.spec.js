b60479031de68e2bbc61bcdaf7665f3cb9d09cf4
Abhinav <abhinav.manchanda@gmail.com>
2017-12-11 22:17:12 +0530

    <Solution> Mocked the service and fixed the test for the controller.

--- test/spec/movie-list/movie-list.component.spec.js
+++ test/spec/movie-list/movie-list.component.spec.js
@@ -3,16 +3,36 @@
 describe('MovieListController', function() {
 
   var movieListController;
+  var movieListService;
 
   beforeEach(module('movieList'));
 
   beforeEach(inject(function($componentController) {
-    movieListController = $componentController('movieList');
+    var stubMovieListData = [
+      {
+        name: 'Gunda',
+        snippet: 'Highest rated movie of all time.'
+      }, {
+        name: 'Tabaahi - The Destroyer',
+        snippet: 'Don\'t awaken his dark side.'
+      }, {
+        name: 'Disco Dancer',
+        snippet: 'Too cool for school.'
+      },  {
+        name: 'The Godfather: Part II',
+        snippet: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.'
+      }
+    ]
+    movieListService = {};
+    movieListService.query = function(){
+      return stubMovieListData;
+    };
+    movieListController = $componentController('movieList', {movieListService: movieListService});
   }));
 
-  it('should create a `movies` model with 6 movies', function() {
-    expect(movieListController.movies.length).toBe(6);
-    expect(movieListController.movies[4].name).toBe("Tabaahi - The Destroyer");
+  it('should create a `movies` model with 4 movies', function() {
+    expect(movieListController.movies.length).toBe(4);
+    expect(movieListController.movies[3].name).toBe("The Godfather: Part II");
   });
 
   it('should reserve an item when reserve for that item is called', function(){
@@ -31,10 +51,10 @@ describe('MovieListController', function() {
 
   it('should display the count of selected movies', function(){
     var secondMovie = movieListController.movies[1];
-    var fifthMovie = movieListController.movies[4];
+    var fourthMovie = movieListController.movies[3];
     movieListController.toggleItemState(secondMovie);
     expect(movieListController.reservedItemCount()).toBe(1);
-    movieListController.toggleItemState(fifthMovie);
+    movieListController.toggleItemState(fourthMovie);
     expect(movieListController.reservedItemCount()).toBe(2);
   })
 });
