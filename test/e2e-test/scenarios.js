0b7cd0d04ac1ab47852fb2ee288f7de3d8741d64
Abhinav <abhinav.manchanda@gmail.com>
2017-12-17 10:51:03 +0530

    Added a basic scenario.

--- test/e2e-test/scenarios.js
+++ test/e2e-test/scenarios.js
@@ -5,4 +5,9 @@ describe('Movie Rental Application', function() {
   beforeEach(function() {
     browser.get('index.html');
   });
+
+  it("should display the list of movies", function(){
+    var movieList = element.all(by.repeater('movie in $ctrl.movies'));
+    expect(movieList.count()).toBe(7);
+  });
 });
